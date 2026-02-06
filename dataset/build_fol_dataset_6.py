8846dfa762cb0ac9e3ea0b42433c1eb125e6c747
andreaspagnolo17 <a.spagnolo9@campus.unimib.it>
2026-02-06 17:17:24 +0100

    fix dimension of the dataset

--- dataset/build_fol_dataset_6.py
+++ dataset/build_fol_dataset_6.py
@@ -35,8 +35,8 @@ cli = ArgParser()
 class DataProcessConfig(BaseModel):
     output_dir: str = "data/fol_basic"
     seq_len: int = 160
-    num_train: int = 50000
-    num_test: int = 5000
+    num_train: int = 20000
+    num_test: int = 2000
     
     # FOL Specifics
     num_constants: int = 4   # Number of constants active in a sample (domain size)
